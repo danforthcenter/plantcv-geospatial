b43f02d0d7b5f6d010c4a47504c562a39322b6ee
HaleySchuhl <haleyschuhl@gmail.com>
2024-03-07 13:44:08 -0600

    start implementing Spectral_data object

--- plantcv/geospatial/readtifimage.py
+++ plantcv/geospatial/readtifimage.py
@@ -1,6 +1,6 @@
 # Read TIF File
 import rasterio
-
+from plantcv.plantcv.classes import Spectral_data 
 
 def readimage(filepath):
     """Read TIF image from file.
@@ -9,17 +9,24 @@ def readimage(filepath):
     filepath: Path of the TIF image file.
 
     Returns:
-    imagearray: Image object as numpy array.
-    height: Height of digital number array values.
-    width: Width of digital number array values.
-    bands: Total number of bands in the image.
+    spectral_array: PlantCV format Hyperspectral data instance
 
     :param filepath: str
-    :return imagearray: numpy.ndarray, height: int, width: int, bands: int
+    :return spectral_array: __main__.Spectral_data
     """
     img = rasterio.open(filepath)
     inputarray = img.read()
     height = img.height
     width = img.width
     bands = img.count
-    return inputarray, height, width, bands
+    spectral_array = Spectral_data(array_data=inputarray,
+                                   max_wavelength=float(str(header_dict["wavelength"][-1]).rstrip()),
+                                   min_wavelength=float(str(header_dict["wavelength"][0]).rstrip()),
+                                   max_value=max(inputarray), min_value=min(inputarray),
+                                   d_type=header_dict["datatype"],
+                                   wavelength_dict=wavelength_dict, samples=int(width),
+                                   lines=int(height), interleave=header_dict["interleave"],
+                                   wavelength_units="unknown", array_type="datacube",
+                                   pseudo_rgb=None, filename=filepath, default_bands=None)
+
+    return spectral_array
