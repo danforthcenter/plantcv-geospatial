bb3aa5bdc5f11cfd4cf98f5de72b96f9fd989905
HaleySchuhl <haleyschuhl@gmail.com>
2024-03-26 16:36:30 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,21 @@
+import os
+import pytest
 import matplotlib
 
 # Disable plotting
 matplotlib.use("Template")
+
+class TestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
+        # Flat image directory
+        self.snapshot_dir = os.path.join(self.datadir, "snapshot_dir")
+        # RGB image
+        self.cropped_tif = os.path.join(self.datadir, "615.tif")
+
+@pytest.fixture(scope="session")
+def test_data():
+    """Test data object for the main PlantCV-geospatial package."""
+    return TestData()
