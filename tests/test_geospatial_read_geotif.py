009999190f8ec5bef319998cdeac342a9f99f7be
HaleySchuhl <haleyschuhl@gmail.com>
2024-03-27 16:05:26 -0500

    add bad input test

--- tests/test_geospatial_read_geotif.py
+++ tests/test_geospatial_read_geotif.py
@@ -1,6 +1,7 @@
 """Tests for geospatial.readd_geotif."""
 
 import cv2
+import pytest
 import plantcv.geospatial.read_geotif as read_geotif
 
 
@@ -9,3 +10,10 @@ def test_geospatial_read_geotif(test_data):
     # read in small 5-band tif image 
     img = read_geotif(filename=test_data.cropped_tif, bands="B,G,R,RE,N")
     assert img.array_data.shape[2] == 5
+
+
+def test_geospatial_read_geotif_bad_input(test_data):
+    """Test for plantcv-geospatial."""
+    # read in small 5-band tif image 
+    with pytest.raises(RuntimeError):
+        img = read_geotif(filename=test_data.cropped_tif, bands="p")
