bc4e22cf4c5a147afcd126616ebac919aabcbd72
Noah Fahlgren <noahfahlgren@gmail.com>
2024-05-18 18:26:54 -0500

    Restore versioning removed in PR

--- plantcv/geospatial/__init__.py
+++ plantcv/geospatial/__init__.py
@@ -1,4 +1,8 @@
+from importlib.metadata import version
 from plantcv.geospatial.read_geotif import read_geotif
+
+# Auto versioning
+__version__ = version("plantcv-geospatial")
 
 __all__ = [
     "read_geotif"
