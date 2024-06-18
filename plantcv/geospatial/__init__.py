b09b8cde1510e84d813b0c657ba2aa34777e59a6
Dhiraj Srivastava <dhiraj.srivastava.mec16@itbhu.ac.in>
2024-06-18 15:01:45 -0500

    Update __init__.py

--- plantcv/geospatial/__init__.py
+++ plantcv/geospatial/__init__.py
@@ -1,5 +1,6 @@
 from importlib.metadata import version
 from plantcv.geospatial.read_geotif import read_geotif
+from plantcv.geospatial.plot_extraction import write_shapefile
 
 # Auto versioning
 __version__ = version("plantcv-geospatial")
