a1023b465876b7499c91c5748bedd22692837a59
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-01 14:05:16 -0500

    Update test_geospatial_transform_points.py
    add read step also

--- tests/test_geospatial_transform_points.py
+++ tests/test_geospatial_transform_points.py
@@ -1,10 +1,12 @@
 """Tests for geospatial.readd_geotif."""
 
 import pytest
-from plantcv.geospatial import transform_points
+from plantcv.geospatial import read_geotif, transform_points
 
 
 def test_geospatial_transform_points(test_data):
     """Test for plantcv-geospatial."""
     # read in small 5-band tif image
-    coords = transform_points(img=test_data.cropped_tif, geojson=test_data.pts_geojson)
+    img = read_geotif(filename=test_data.cropped_tif, bands="B,G,R,RE,N")
+    coords = transform_points(img=img, geojson=test_data.pts_geojson)
+    assert coords == []
