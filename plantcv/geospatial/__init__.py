adc2099f9fd126e5a8f3581678ebaf5997b2cf5f
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-01 14:02:09 -0500

    Update __init__.py

--- plantcv/geospatial/__init__.py
+++ plantcv/geospatial/__init__.py
@@ -1,9 +1,11 @@
 from importlib.metadata import version
 from plantcv.geospatial.read_geotif import read_geotif
+from plantcv.geospatial.transform_points import transform_points
 
 # Auto versioning
 __version__ = version("plantcv-geospatial")
 
 __all__ = [
-    "read_geotif"
+    "read_geotif",
+    "transform_points"
     ]
