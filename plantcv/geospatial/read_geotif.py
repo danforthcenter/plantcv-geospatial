d85a8038df5ab700af4683262ea73d6e8d9068ec
HaleySchuhl <haleyschuhl@gmail.com>
2024-09-04 13:00:03 -0500

    syntax cleanup in read_geotif.py

--- plantcv/geospatial/read_geotif.py
+++ plantcv/geospatial/read_geotif.py
@@ -114,22 +114,21 @@ def read_geotif(filename, bands="R,G,B", cropto=None):
     # Check if user input matches image dimension in z direction
     if depth != len(bands):
         warn(f"{depth} bands found in the image data but {filename} was provided with {bands}")
-        if depth < len(bands):
-            fatal_error(f"your image depth is less than the specified number of bands")
+    if depth < len(bands):
+        fatal_error("your image depth is less than the specified number of bands")
     # Mask negative background values
     img_data[img_data < 0.] = 0
     if np.sum(img_data) == 0:
         fatal_error(f"your image is empty, are the crop-to bounds outside of the {filename} image area?")
-
     # Make a list of wavelength keys
     wl_keys = wavelengths.keys()
     if 0 in wl_keys:
         id_mask = _find_closest_unsorted(array=np.array([float(i) for i in wl_keys]), target=0)
         mask_layer = img_data[:, :, [id_mask]]
         if len(np.unique(mask_layer)) > 2:
-            fatal_error(f"your specified mask is not binary")
-        else:
-            # apply mask
+            fatal_error("your specified mask is not binary")
+        if len(np.unique(mask_layer)) <= 2:
+            # Apply mask
             img_data = np.where(mask_layer == 0, 0, img_data)
     # Find which bands to use for red, green, and blue bands of the pseudo_rgb image
     id_red = _find_closest_unsorted(array=np.array([float(i) for i in wl_keys]), target=630)
