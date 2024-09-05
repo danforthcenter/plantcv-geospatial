4d4ce2e14bfe1a04f8fb9edb07c40ce7d2657265
HaleySchuhl <haleyschuhl@gmail.com>
2024-09-05 10:02:28 -0500

    update test names

--- tests/test_geospatial_transform_polygons.py
+++ tests/test_geospatial_transform_polygons.py
@@ -3,7 +3,7 @@
 from plantcv.geospatial import read_geotif, transform_polygons
 
 
-def test_geospatial_transform_points(test_data):
+def test_geospatial_transform_polygons(test_data):
     """Test for plantcv-geospatial."""
     # read in small 5-band tif image
     img = read_geotif(filename=test_data.rgb_tif, bands="B,G,R")
@@ -11,7 +11,7 @@ def test_geospatial_transform_points(test_data):
     assert coords == [[[196, 115], [145, 78], [114, 120], [165, 157]]]
 
 
-def test_geospatial_transform_single_points(test_data):
+def test_geospatial_transform_single_polygons(test_data):
     """Test for plantcv-geospatial."""
     # read in small 5-band tif image
     img = read_geotif(filename=test_data.rgb_tif, bands="B,G,R")
