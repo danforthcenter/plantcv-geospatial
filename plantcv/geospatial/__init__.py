e8ebc7a7104cc2200a022f63629c51ed29c4aa40
HaleySchuhl <haleyschuhl@gmail.com>
2024-10-04 11:58:24 -0500

    Update __init__.py

--- plantcv/geospatial/__init__.py
+++ plantcv/geospatial/__init__.py
@@ -2,6 +2,7 @@ from importlib.metadata import version
 from plantcv.geospatial.read_geotif import read_geotif
 from plantcv.geospatial.transform_points import transform_points
 from plantcv.geospatial.transform_polygons import transform_polygons
+from plantcv.geospatial.points2roi import points2roi_circle
 
 # Auto versioning
 __version__ = version("plantcv-geospatial")
@@ -9,5 +10,6 @@ __version__ = version("plantcv-geospatial")
 __all__ = [
     "read_geotif",
     "transform_points",
-    "transform_polygons"
+    "transform_polygons",
+    "points2roi_circle"
     ]
