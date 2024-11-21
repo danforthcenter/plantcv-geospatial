05f128ab769070d4612456ae3e4dbc6362e21e07
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-21 11:11:06 -0600

    add test and data for geojson with ID properties

--- tests/analyze/test_geospatial_analyze_mask.py
+++ tests/analyze/test_geospatial_analyze_mask.py
@@ -13,3 +13,13 @@ def test_mask(test_data):
     bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
     _ = analyze_mask(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
     assert outputs.observations["default_0"]["percent_coverage"]["value"] <= 1
+    
+def test_mask_with_geo_ids(test_data):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    # Read in test data
+    img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
+    bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
+    _ = analyze_mask(img=img, bin_mask=bin_mask, geojson=test_data.geojson_with_id)
+    assert outputs.observations["888"]["percent_coverage"]["value"] <= 1
