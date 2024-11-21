4b05e4b906227ec7fdd8ae641933ae5dd83aafbd
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-21 10:25:44 -0600

    Create test_geospatial_analyze_mask.py

---
+++ tests/analyze/test_geospatial_analyze_mask.py
@@ -0,0 +1,16 @@
+import numpy as np
+from plantcv.plantcv import outputs
+from plantcv.geospatial.analyze import mask as analyze_mask
+from plantcv.geospatial import read_geotif
+
+
+def test_mask(test_data):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    # Read in test data
+    img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
+    bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
+    _ = analyze_mask(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
+    print(outputs.observations)
+    assert outputs.observations["default_0"]["percent_coverage"]["value"] <= 1
