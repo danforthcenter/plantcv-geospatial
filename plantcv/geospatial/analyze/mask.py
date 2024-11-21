5f2c2293d4c0c74be77c5d888c45a3a2e60d2bbb
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-21 10:25:36 -0600

    fix indent, and update percent cover

--- plantcv/geospatial/analyze/mask.py
+++ plantcv/geospatial/analyze/mask.py
@@ -22,31 +22,35 @@ def mask(img, bin_mask, geojson):
     :return analysis_image: numpy.ndarray
     """
 
-# sum gives the sum of pixel values, so change from [0,255] to [0,1] 
-bin_mask = bin_mask.astype(float) / 255
-all_ones = np.ones(bin_mask.shape)
-affine = img.metadata["transform"]
-# Vectorized (efficient) data extraction of pixel count per sub-region
-region_counts = zonal_stats(geojson, bin_mask, affine=affine, stats="sum")
+    # sum gives the sum of pixel values, so change from [0,255] to [0,1] 
+    bin_mask = bin_mask.astype(float) / 255
+    all_ones = np.ones(bin_mask.shape[:2])
+    affine = img.metadata["transform"]
+    # Vectorized (efficient) data extraction of pixel count per sub-region
+    region_counts = zonal_stats(geojson, bin_mask, affine=affine, stats="sum")
 
-total_region = zonal_stats(geojson, all_ones, affine=affine, stats="sum")
+    total_region = zonal_stats(geojson, all_ones, affine=affine, stats="sum")
 
-# If IDs within the geojson
-ids = []
-# Gather list of IDs
-with fiona.open(geojson, 'r') as shapefile:
-    for row in shapefile:
-        temp_list = []
-        # Polygon
-        if len(row.geometry["coordinates"][0]) > 1:
-            square = row.geometry["coordinates"][0][:-1]
-            ids.append((row['properties']["ID"]))
+    # If IDs within the geojson
+    ids = []
+    # Gather list of IDs
+    with fiona.open(geojson, 'r') as shapefile:
+        for i, row in enumerate(shapefile):
+            temp_list = []
+            # Polygon
+            if len(row.geometry["coordinates"][0]) > 1:
+                square = row.geometry["coordinates"][0][:-1]
+                if "ID" in row['properties']:
+                    ids.append((row['properties']["ID"]))
+                else:
+                    # If there are no IDs in the geojson then use default labels
+                    ids.append("default_" + str(i))
 
-# Save data to outputs as custom observation (since using data extraction method not yet integrated into the repo) 
-for i, id_lbl in enumerate(ids):
-    outputs.add_observation(sample=id_lbl, variable="pixel_count", trait="count",
-                            method="rasterstats.zonal_stats", scale="pixels", datatype=int,
-                            value=region_counts[i]["sum"], label="pixels")
-    outputs.add_observation(sample=id_lbl, variable="percent_coverage", trait="percentage",
-                            method="rasterstats.zonal_stats", scale="none", datatype=float,
-                            value=total_region[i]["sum"], label="none")
+    # Save data to outputs as custom observation (since using data extraction method not yet integrated into the repo) 
+    for i, id_lbl in enumerate(ids):
+        outputs.add_observation(sample=id_lbl, variable="pixel_count", trait="count",
+                                method="rasterstats.zonal_stats", scale="pixels", datatype=int,
+                                value=region_counts[i]["sum"], label="pixels")
+        outputs.add_observation(sample=id_lbl, variable="percent_coverage", trait="percentage",
+                                method="rasterstats.zonal_stats", scale="none", datatype=float,
+                                value=region_counts[i]["sum"]/total_region[i]["sum"], label="none")
