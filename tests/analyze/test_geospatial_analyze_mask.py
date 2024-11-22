e732e2e8e4b02a4e3e0becf3f66a532315291946
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-22 11:18:59 -0600

    Update test_geospatial_analyze_mask.py

--- tests/analyze/test_geospatial_analyze_mask.py
+++ tests/analyze/test_geospatial_analyze_mask.py
@@ -1,26 +1,26 @@
 import numpy as np
 from plantcv.plantcv import outputs
-from plantcv.geospatial.analyze import mask as analyze_mask
+from plantcv.geospatial.analyze import coverage as analyze_coverage
 from plantcv.geospatial import read_geotif
 
 
-def test_mask(test_data):
+def test_coverage(test_data):
     """Test for PlantCV."""
     # Clear previous outputs
     outputs.clear()
     # Read in test data
     img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
     bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
-    _ = analyze_mask(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
+    _ = analyze_coverage(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
     assert outputs.observations["default_0"]["percent_coverage"]["value"] <= 1
 
 
-def test_mask_with_geo_ids(test_data):
+def test_coverage_with_geo_ids(test_data):
     """Test for PlantCV."""
     # Clear previous outputs
     outputs.clear()
     # Read in test data
     img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
     bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
-    _ = analyze_mask(img=img, bin_mask=bin_mask, geojson=test_data.geojson_with_id)
+    _ = analyze_coverage(img=img, bin_mask=bin_mask, geojson=test_data.geojson_with_id)
     assert outputs.observations["888"]["percent_coverage"]["value"] <= 1
