90160d22c3269287062b602d611954e2c615d831
k034b363 <k034b363@gmail.com>
2024-12-04 16:07:19 -0600

    Fix unmatched parentheses in test

--- tests/test_geospatial_napari_save_points.py
+++ tests/test_geospatial_napari_save_points.py
@@ -1,10 +1,9 @@
 """Tests for geospatial.napari_save_points"""
 
-import pytest
 import os
-import napari
 from plantcv.geospatial import napari_save_points, read_geotif
 from plantcv.plantcv import print_image
+
 
 def test_geospatial_napari_save_points(test_data, tmpdir):
     """Test for plantcv-geospatial."""
@@ -15,14 +14,13 @@ def test_geospatial_napari_save_points(test_data, tmpdir):
     viewer.close()
 
 
-
 def test_geospatial_napari_save_points_output(test_data, tmpdir):
     """Test for plantcv-geospatial."""
     cache_dir = tmpdir.mkdir("cache")
     # test with a png by creating a temp version of the rgb tif in cache_dir
     img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
     print_image(img.pseudo_rgb, os.path.join(cache_dir, "rgb.png"))
-    images = [os.path.join(cache_dir, "rgb.png"]
-    redo_list = napari_save_points(images, num_points=0, outdir=cache_dir, block=False, show=False) 
+    images = [os.path.join(cache_dir, "rgb.png")]
+    _ = napari_save_points(images, num_points=0, outdir=cache_dir, block=False, show=False) 
     assert os.path.exists(os.path.join(cache_dir, "rgb_warp.txt"))
     viewer.close()
