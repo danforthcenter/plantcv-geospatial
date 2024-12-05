ba65ee2089292cd099bdec67d47c42d0b09addaf
HaleySchuhl <haleyschuhl@gmail.com>
2024-12-05 10:34:40 -0600

    update tests to parameterize debugging

--- tests/analyze/test_geospatial_analyze_coverage.py
+++ tests/analyze/test_geospatial_analyze_coverage.py
@@ -1,15 +1,22 @@
-from plantcv.plantcv import outputs
+import pytest
+from plantcv.plantcv import outputs, params
 from plantcv.geospatial.analyze import coverage as analyze_coverage
 from plantcv.geospatial import read_geotif
 
 
-def test_coverage(test_data):
+@pytest.mark.parametrize("debug", ["print", "plot", None])
+def test_coverage(debug, tmpdir, test_data):
     """Test for PlantCV."""
     # Clear previous outputs
     outputs.clear()
+    # Create a tmp directory
+    cache_dir = tmpdir.mkdir("cache")
+    params.debug_outdir = cache_dir
     # Read in test data
     img = read_geotif(filename=test_data.rgb_tif, bands="R,G,B")
     bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
+    # Debug mode
+    params.debug = debug
     _ = analyze_coverage(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
     assert outputs.observations["default_0"]["percent_coverage"]["value"] <= 1
 
