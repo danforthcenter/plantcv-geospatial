9cd8811d6956c7752a3c85aa9568c0c7af8d7620
HaleySchuhl <haleyschuhl@gmail.com>
2024-12-13 08:59:43 -0600

    Create _helpers.py
    add _transform_geojson_crs

---
+++ plantcv/geospatial/_helpers.py
@@ -0,0 +1,27 @@
+# Read and transform georeferenced GeoJSON/shapefile points into image CRS
+import geopandas
+
+
+def _transform_geojson_crs(img, geojson):
+    """
+    Helper function for opening and transforming Coordinate System
+    of a geojson/shapefile
+
+    Keyword inputs:
+    Inputs:
+    img:        A spectral object from read_geotif.
+    geojson:    Path to the shapefile.
+
+    :param img: [spectral object]]
+    :return geojson: str
+    :return gdf: geopandas
+    """
+    gdf = geopandas.read_file(geojson)
+
+    img_crs = img.metadata['crs']
+
+    # Check spectral object and geojson have the same CRS, if not then convert
+    if not gdf.crs == img_crs:
+        gdf = gdf.to_crs(epsg=img_crs)
+
+    return gdf
