6208aac2c867af505333656001a83e6597f333fd
k034b363 <k034b363@gmail.com>
2025-01-21 10:30:45 -0600

    Changed read_geotif to joblib.load in all tests

--- tests/test_geospatial_points2roicircle.py
+++ tests/test_geospatial_points2roicircle.py
@@ -1,12 +1,13 @@
 """Tests for geospatial.points2roi_circle"""
 
-from plantcv.geospatial import read_geotif, points2roi_circle
 import numpy as np
+import joblib
+from plantcv.geospatial import points2roi_circle
 
 
 def test_geospatial_points2roi_circle(test_data):
     """Test for plantcv-geospatial."""
     # read in small 3-band tif image
-    img = read_geotif(filename=test_data.rgb_tif, bands="B,G,R")
+    img = joblib.load(test_data.rgb_pickled)
     rois = points2roi_circle(img=img, geojson=test_data.pts_geojson, radius=0.5)
     assert np.all(rois.contours[0][0][0] == np.array([119, 170]))
