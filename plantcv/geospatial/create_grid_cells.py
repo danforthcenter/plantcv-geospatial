4eaed7ce591055f7e99352b1ca26b969a1684ad3
HaleySchuhl <haleyschuhl@gmail.com>
2025-02-06 10:58:15 -0600

    utilize helpers in gridcell functions, rename direction vectors

--- plantcv/geospatial/create_grid_cells.py
+++ plantcv/geospatial/create_grid_cells.py
@@ -1,6 +1,7 @@
-#
+# Create rectangular geojsons
+
 from shapely.geometry import LineString, Polygon, mapping
-from plantcv.geospatial._helpers import _unpack_point_shapefiles
+from plantcv.geospatial._helpers import _unpack_point_shapefiles, _calc_direction_vectors
 import fiona
 
 
@@ -28,31 +29,8 @@ def create_polygons(four_points_path, plot_geojson_path, out_path,
     list
         List of dictionaries containing the created grid cell polygons
     """
-    # Read the four corner points shapefile
-    with fiona.open(four_points_path, 'r') as shapefile:
-        # Unpack coordinates regardless of Multi-Point or Point geometry type
-        coordinates = _unpack_point_shapefiles(shapefile)
-        crs = shapefile.crs
-        driver = shapefile.driver
-        schema = {
-            'geometry': 'Polygon',
-            'properties': {}
-        }
-
-    # Read the plot boundaries shapefile
-    with fiona.open(plot_geojson_path, 'r') as shapefile:
-        plot_corner_points = _unpack_point_shapefiles(shapefile)
-
-    # Create LineString objects for edges
-    # NOTE: order will depend on order that 4 corners are clicked in
-    edge_1 = LineString([coordinates[1][0], coordinates[2][0]])  # vertical edge
-    edge_2 = LineString([coordinates[0][0], coordinates[1][0]])  # horizontal edge
-
-    # Calculate direction vectors
-    edge_1_dir = ((edge_1.coords[1][0] - edge_1.coords[0][0]) / edge_1.length,
-                  (edge_1.coords[1][1] - edge_1.coords[0][1]) / edge_1.length)
-    edge_2_dir = ((edge_2.coords[1][0] - edge_2.coords[0][0]) / edge_2.length,
-                  (edge_2.coords[1][1] - edge_2.coords[0][1]) / edge_2.length)
+    # Calculate direction vectors based on plot boundaries
+    horizontal_dir, vertical_dir, _ = _calc_direction_vectors(plot_bounds=four_points_path)
 
     # Cell width
     horizontal_threshold = horizontal_length
@@ -61,20 +39,20 @@ def create_polygons(four_points_path, plot_geojson_path, out_path,
     grid_cells = []
 
     # Create grid cells for each plot
-    for points in plot_corner_points:
+    for points in plot_geojson_path:
         for column_number in range(horizontal_cells):
             # Calculate corners of each grid cell
-            bottom_left = (points[0][0] + column_number * horizontal_threshold * edge_2_dir[0],
-                           points[0][1] + column_number * horizontal_threshold * edge_2_dir[1])
+            bottom_left = (points[0][0] + column_number * horizontal_threshold * horizontal_dir[0],
+                           points[0][1] + column_number * horizontal_threshold * horizontal_dir[1])
 
-            bottom_right = (bottom_left[0] + horizontal_threshold * edge_2_dir[0],
-                            bottom_left[1] + horizontal_threshold * edge_2_dir[1])
+            bottom_right = (bottom_left[0] + horizontal_threshold * horizontal_dir[0],
+                            bottom_left[1] + horizontal_threshold * horizontal_dir[1])
 
-            top_left = (bottom_left[0] + vertical_length * edge_1_dir[0],
-                        bottom_left[1] + vertical_length * edge_1_dir[1])
+            top_left = (bottom_left[0] + vertical_length * vertical_dir[0],
+                        bottom_left[1] + vertical_length * vertical_dir[1])
 
-            top_right = (bottom_right[0] + vertical_length * edge_1_dir[0],
-                         bottom_right[1] + vertical_length * edge_1_dir[1])
+            top_right = (bottom_right[0] + vertical_length * vertical_dir[0],
+                         bottom_right[1] + vertical_length * vertical_dir[1])
 
             # Create polygon from corners
             cell = Polygon([bottom_left, bottom_right, top_right, top_left, bottom_left])
@@ -118,51 +96,29 @@ def create_grid_cells(four_points_path, out_path, alley_size, num_ranges, num_pl
     list
         List of dictionaries containing the created grid cell polygons
     """
-    # Read the four corner points shapefile
-    with fiona.open(four_points_path, 'r') as shapefile:
-        # Unpack coordinates regardless of Multi-Point or Point geometry type
-        coordinates = _unpack_point_shapefiles(shapefile)
-        crs = shapefile.crs
-        driver = shapefile.driver
-        schema = {
-            'geometry': 'Polygon',
-            'properties': {}
-        }
-
-    # Create LineString objects for edges
-    # NOTE: order will depend on order that 4 corners are clicked in
-    edge_1 = LineString([coordinates[1][0], coordinates[2][0]])  # vertical edge
-    edge_2 = LineString([coordinates[0][0], coordinates[1][0]])  # horizontal edge
-
-    # Calculate direction vectors
-    edge_1_dir = ((edge_1.coords[1][0] - edge_1.coords[0][0]) / edge_1.length,
-                  (edge_1.coords[1][1] - edge_1.coords[0][1]) / edge_1.length)
-    edge_2_dir = ((edge_2.coords[1][0] - edge_2.coords[0][0]) / edge_2.length,
-                  (edge_2.coords[1][1] - edge_2.coords[0][1]) / edge_2.length)
+    # Calculate direction vectors based on plot boundaries
+    horizontal_dir, vertical_dir, anchor_point = _calc_direction_vectors(plot_bounds=four_points_path)
 
     # Initialize list for storing grid cells
     grid_cells = []
 
-    # Initialize first coordinate of the grid 
-    anchor_point = coordinates[0] ## AKA bottom_left
-    
     # Create grid cells for each plot
     for range_number in range(num_ranges):
         for column_number in range(num_plots):
             # Calculate corners of each grid cell
-            bottom_left = (anchor_point[0][0] + column_number * horizontal_length * edge_2_dir[0],
-                           anchor_point[0][1] + column_number * horizontal_length * edge_2_dir[1] +
+            bottom_left = (anchor_point[0][0] + column_number * horizontal_length * horizontal_dir[0],
+                           anchor_point[0][1] + column_number * horizontal_length * horizontal_dir[1] +
                            # Include alley spacing in range location 
-                           range_number * (vertical_length + alley_size) * edge_1_dir[1])
+                           range_number * (vertical_length + alley_size) * vertical_dir[1])
 
-            bottom_right = (bottom_left[0] + horizontal_length * edge_2_dir[0],
-                            bottom_left[1] + horizontal_length * edge_2_dir[1])
+            bottom_right = (bottom_left[0] + horizontal_length * horizontal_dir[0],
+                            bottom_left[1] + horizontal_length * horizontal_dir[1])
 
-            top_left = (bottom_left[0] + vertical_length * edge_1_dir[0],
-                        bottom_left[1] + vertical_length * edge_1_dir[1])
+            top_left = (bottom_left[0] + vertical_length * vertical_dir[0],
+                        bottom_left[1] + vertical_length * vertical_dir[1])
 
-            top_right = (bottom_right[0] + vertical_length * edge_1_dir[0],
-                         bottom_right[1] + vertical_length * edge_1_dir[1])
+            top_right = (bottom_right[0] + vertical_length * vertical_dir[0],
+                         bottom_right[1] + vertical_length * vertical_dir[1])
 
             # Create polygon from corners
             cell = Polygon([bottom_left, bottom_right, top_right, top_left, bottom_left])
