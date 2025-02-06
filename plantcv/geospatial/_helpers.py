7ffea2059feefc56ffde60473a1c89b69de9f25c
HaleySchuhl <haleyschuhl@gmail.com>
2025-02-06 10:57:47 -0600

    add _calc_direction_vectors to _helpers.py

--- plantcv/geospatial/_helpers.py
+++ plantcv/geospatial/_helpers.py
@@ -1,5 +1,7 @@
 # PlantCV-geospatial helper functions
 import geopandas
+from shapely.geometry import LineString
+import fiona
 
 
 def _transform_geojson_crs(img, geojson):
@@ -43,3 +45,51 @@ def _unpack_point_shapefiles(shapefile):
     if type(shapefile[0]['geometry']['coordinates']) is tuple:
         coordinates = [[shape['geometry']['coordinates']] for shape in shapefile]
     return coordinates
+
+
+def _calc_direction_vectors(plot_bounds):
+    """
+    Helper function for opening plot boundaries and calculating direction vectors
+
+    Keyword inputs:
+    Inputs:
+    plot_bounds: path to Fiona formatted shapefile data of the plot boundary
+
+    Returns:
+    --------
+    horizontal_dir
+        Direction vector in the horizontal direction
+    vertical_dir
+        Direction vector in the vertical direction
+    anchor_point
+        First coordinate in the boundary shapefile
+
+    :param plot_bounds: str
+    :return horizontal_dir: list
+    :return vertical_dir: list
+    :return anchor_point: tuple
+    """
+    # Read the four corner points shapefile
+    with fiona.open(plot_bounds, 'r') as shapefile:
+        # Unpack coordinates regardless of Multi-Point or Point geometry type
+        coordinates = _unpack_point_shapefiles(shapefile)
+        crs = shapefile.crs
+        driver = shapefile.driver
+        schema = {
+            'geometry': 'Polygon',
+            'properties': {}
+        }
+
+    # Create LineString objects for edges
+    # NOTE: order will depend on order that 4 corners are clicked in
+    edge_1 = LineString([coordinates[1][0], coordinates[2][0]])
+    edge_2 = LineString([coordinates[0][0], coordinates[1][0]])
+
+    # Calculate direction vectors
+    vertical_dir = ((edge_1.coords[1][0] - edge_1.coords[0][0]) / edge_1.length,
+                    (edge_1.coords[1][1] - edge_1.coords[0][1]) / edge_1.length)
+    horizontal_dir = ((edge_2.coords[1][0] - edge_2.coords[0][0]) / edge_2.length,
+                      (edge_2.coords[1][1] - edge_2.coords[0][1]) / edge_2.length)
+    anchor_point = coordinates[0]
+
+    return horizontal_dir, vertical_dir, anchor_point
