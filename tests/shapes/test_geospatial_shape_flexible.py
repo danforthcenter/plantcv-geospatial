89be2a1d64f5ca02c8af0ab54a6e772fb513e18f
HaleySchuhl <haleyschuhl@gmail.com>
2025-02-10 10:16:05 -0600

    update unit test to parameterize debug for code coverage

--- tests/shapes/test_geospatial_shape_flexible.py
+++ tests/shapes/test_geospatial_shape_flexible.py
@@ -1,16 +1,21 @@
 """Tests for geospatial.transform_points"""
 
 from plantcv.geospatial.shapes import flexible as shape_flexible
+from plantcv.plantcv import params
+import pytest
 import joblib
 import os
 
 
-def test_geospatial_shape_flexible(test_data, tmpdir):
+@pytest.mark.parametrize("debug", ["print", "plot", None])
+def test_geospatial_shape_flexible(debug, test_data, tmpdir):
     """Test for plantcv-geospatial."""
     cache_dir = tmpdir.mkdir("cache")
     filename = os.path.join(cache_dir, 'test_out.geojson')
     # Read in test data
     img = joblib.load(test_data.rgb_pickled)
+    # Debug mode
+    params.debug = debug
     _ = shape_flexible(img=img, field_corners=test_data.plot_bounds, plot_geojson_path=test_data.plot_points, out_path=filename,
                        num_rows=8, range_length=4, column_length=0.5)
     assert os.path.exists(filename)
