40bb26a856f0b759c4bda420fcef329492464d47
HaleySchuhl <haleyschuhl@gmail.com>
2025-02-19 08:05:19 -0600

    testing add_stat option in zonal_stats
    ability to add histogram calculation as custom output from vectorized operation? testing

--- plantcv/geospatial/analyze/spectral.py
+++ plantcv/geospatial/analyze/spectral.py
@@ -2,6 +2,7 @@
 from rasterstats import zonal_stats
 from plantcv.plantcv import outputs, params
 from plantcv.plantcv.visualize import pseudocolor
+from plantcv.plantcv.visualize.histogram import _hist_gray
 from matplotlib import pyplot as plt
 from rasterio.plot import plotting_extent
 import geopandas
@@ -9,7 +10,36 @@ import fiona
 import os
 
 
-def spectral(img, geojson):
+import numpy as np
+
+
+class Image(np.ndarray):
+    """Generic image class that extends the np.ndarray class."""
+
+    # From NumPy documentation
+    # Add uri attribute
+    def __new__(cls, input_array: np.ndarray, uri: str):
+        obj = np.asarray(input_array).view(cls)
+        # New attribute uri stores uniform resource identifier of the source file
+        obj.uri = uri
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is not None:
+            self.uri = getattr(obj, "uri", None)
+
+    def __getitem__(self, key):
+        # Enhance the np.ndarray __getitem__ method
+        # Slice the array as requested but return an array of the same class
+        # Idea from NumPy examples of subclassing:
+        return super(Image, self).__getitem__(key)
+
+
+def spectral_grab(x, props=None):
+    print(props)
+
+
+def spectral(img, geojson, bins=10):
     """A function that analyzes pixel intensity values for a spectral index
     Inputs:
     img          = Spectral_Data index object of geotif data, used for analysis
@@ -25,14 +55,18 @@ def spectral(img, geojson):
     bin_label = []
     affine = img.metadata["transform"]
 
-    # Vectorized (efficient) data extraction of spectral signature per sub-region
-    stats = zonal_stats(geojson, img.array_data, affine=affine,
-                        stats=['mean', 'median', 'std', 'count'], nodata=-9999)
+    
 
     # If IDs within the geojson
     ids = []
     # Gather list of IDs
     with fiona.open(geojson, 'r') as shapefile:
+        ## Add properties to the geojson object, and then should be able to access inside the function called in add_stats
+        # Vectorized (efficient) data extraction of spectral signature per sub-region
+        stats = zonal_stats(shapefile, img.array_data, affine=affine,
+                        stats=['mean', 'median', 'std', 'percentile_50'], 
+                        add_stats={'hist': spectral_grab }, nodata=-9999)
+    
         for i, row in enumerate(shapefile):
             if 'ID' in row['properties']:
                 label = ((row['properties']["ID"]))
@@ -56,37 +90,38 @@ def spectral(img, geojson):
 
             outputs.add_observation(sample=label, variable=f"index_frequencies_{img.array_type}",
                                     trait="index frequencies", method="plantcv.geospatial.analyze.spectral", scale="frequency",
-                                    datatype=list, value=stats[i]['count'], label=bin_label)
+                                    datatype=list, value=stats[i]['percentile_50'], label=bin_label)
 
     bounds = geopandas.read_file(geojson)
 
     # Plot the GeoTIFF
     # Make a flipped image for graphing
-    vis = pseudocolor(gray_img=img.array_data, min_value=img.array_data.min, max_value=img.array_data.max)
+    #vis = pseudocolor(gray_img=img.array_data, min_value=img.array_data.min, max_value=img.array_data.max)
 
-    _, ax = plt.subplots(figsize=(10, 10))
-    fig_extent = plotting_extent(img.array_data[:, :, :3],
-                                 img.metadata['transform'])
-    ax.imshow(vis, extent=fig_extent)
-    # Plot the shapefile
-    bounds.boundary.plot(ax=ax, color="red")
-    # Set plot title and labels
-    plt.title("Shapefile on GeoTIFF")
-    plt.xlabel("Longitude")
-    plt.ylabel("Latitude")
-    # Store the plot
-    plotting_img = plt.gcf()
+    # _, ax = plt.subplots(figsize=(10, 10))
+    # fig_extent = plotting_extent(img.array_data[:, :, :3],
+    #                              img.metadata['transform'])
+    # ax.imshow(vis, extent=fig_extent)
+    # # Plot the shapefile
+    # bounds.boundary.plot(ax=ax, color="red")
+    # # Set plot title and labels
+    # plt.title("Shapefile on GeoTIFF")
+    # plt.xlabel("Longitude")
+    # plt.ylabel("Latitude")
+    # # Store the plot
+    # plotting_img = plt.gcf()
 
-    # Print or plot if debug is turned on
-    if params.debug is not None:
-        if params.debug == 'print':
-            plt.savefig(os.path.join(params.debug_outdir, str(
-                params.device) + '_analyze_coverage.png'), dpi=params.dpi)
-            plt.close()
-        elif params.debug == 'plot':
-            # Use non-blocking mode in case the function is run more than once
-            plt.show(block=False)
-    else:
-        plt.close()
+    # # Print or plot if debug is turned on
+    # if params.debug is not None:
+    #     if params.debug == 'print':
+    #         plt.savefig(os.path.join(params.debug_outdir, str(
+    #             params.device) + '_analyze_coverage.png'), dpi=params.dpi)
+    #         plt.close()
+    #     elif params.debug == 'plot':
+    #         # Use non-blocking mode in case the function is run more than once
+    #         plt.show(block=False)
+    # else:
+    #     plt.close()
 
-    return plotting_img
+    # return plotting_img
+    return stats
