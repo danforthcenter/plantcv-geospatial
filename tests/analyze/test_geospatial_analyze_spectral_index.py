47f3051584d32272b44869789415a0d120c1facb
HaleySchuhl <haleyschuhl@gmail.com>
2025-08-27 11:24:38 -0500

    Create test_geospatial_analyze_spectral_index.py

---
+++ tests/analyze/test_geospatial_analyze_spectral_index.py
@@ -0,0 +1,24 @@
+import pytest
+import joblib
+from plantcv.plantcv import outputs, params
+from plantcv.geospatial.analyze import spectral_index as analyze_spectral
+
+
+@pytest.mark.parametrize("debug", [None])
+def test_analyze_spectral(debug, tmpdir, test_data):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    # Create a tmp directory
+    cache_dir = tmpdir.mkdir("cache")
+    params.debug_outdir = cache_dir
+    # Read in test data
+    img = joblib.load(test_data.multi_pickled)
+    img.metadata['nodata'] = 0
+    img.array_data = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
+
+    # Debug mode
+    params.debug = debug
+    _ = analyze_spectral(img=img, geojson=test_data.multipolygon)
+    assert outputs.observations["default_0"]["percentile_75_datacube"]["value"] <= 1
+
