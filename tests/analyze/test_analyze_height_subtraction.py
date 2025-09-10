7fbd2fa3ac36f69fd3fcc3a3b16999488ea53eb1
Eliza Seigel <eseigel@danforthcenter.org>
2025-09-10 15:14:21 -0500

    Update test_analyze_height_subtraction.py
    changing assert to np.sum to avoid failed check

--- tests/analyze/test_analyze_height_subtraction.py
+++ tests/analyze/test_analyze_height_subtraction.py
@@ -2,6 +2,7 @@
 
 import joblib
 import pytest
+import numpy as np
 from plantcv.plantcv import outputs, params
 from plantcv.geospatial.analyze import height_subtraction
 
@@ -15,7 +16,7 @@ def test_height_subtraction(test_data):
     # Read in test data
     dsm = joblib.load(test_data.rgb_pickled)
     test = height_subtraction(dsm1=dsm, dsm0=dsm)
-    assert sum(test) == 0
+    assert np.sum(test, axis=0) == 0
 
 def test_height_subtraction_unequal_crs(test_data):
     """Test for PlantCV."""
