93044cba8fdb6386afe92c115536187af49be59b
Eliza Seigel <eseigel@danforthcenter.org>
2025-09-10 14:14:54 -0500

    Update and rename test.analyze.height_subtraction.py to test_analyze.height_subtraction.py
    Adding check for CRS in test

---
+++ tests/analyze/test_analyze.height_subtraction.py
@@ -0,0 +1,33 @@
+"""Tests for geospatial.analyze.height_subtraction"""
+
+import joblib
+from plantcv.plantcv import outputs, params
+from plantcv.geospatial.analyze import height_subtraction
+
+
+def test_height_subtraction(testdata):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    # Debug mode
+    params.debug = "plot"
+    # Read in test data
+    dsm = joblib.load(test_data.rgb_pickled)
+    test = height_subtraction(dsm1=dsm, dsm0=dsm)
+    assert sum(test.arraydata) == 0
+
+def test_height_subtraction(testdata):
+    """Test for PlantCV."""
+    with pytest.raises(RuntimeError):
+    # Clear previous outputs
+    outputs.clear()
+    # Debug mode
+    params.debug = "plot"
+    # Read in test data
+    dsm1_fake = joblib.load(test_data.rgb_pickled)
+    dsm0_fake = joblib.load(test_data.rgb_pickled)
+    # Overwriting CRS
+    dsm1_fake.metadata["crs"] = 0
+    with pytest.raises(RuntimeError):
+        test = height_subtraction(dsm1=dsm1_fake, dsm0=dsm0_fake)
+        
