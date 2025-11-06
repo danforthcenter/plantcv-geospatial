e36678137ad0fb8321f0150e93fd00ac9c35416d
HaleySchuhl <haleyschuhl@gmail.com>
2025-11-06 14:02:53 -0600

    add ability to modify label in analyze.coverage

--- plantcv/geospatial/analyze/coverage.py
+++ plantcv/geospatial/analyze/coverage.py
@@ -1,11 +1,11 @@
 # Analyze pixel count over many regions
 from plantcv.geospatial._helpers import _gather_ids, _show_geojson
-from plantcv.plantcv import outputs
+from plantcv.plantcv import outputs, params
 from rasterstats import zonal_stats
 import numpy as np
 
 
-def coverage(img, bin_mask, geojson):
+def coverage(img, bin_mask, geojson, label=None):
     """A function that analyzes pixel coverage in a binary mask and outputs data.
     Inputs:
     img          = Spectral_Data object of geotif data, used for affine metadata
@@ -20,6 +20,10 @@ def coverage(img, bin_mask, geojson):
     :param geojson: str
     :return analysis_image: numpy.ndarray
     """
+    # Set lable to params.sample_label if None
+    if label is None:
+        label = params.sample_label
+        
     # zonal_stats "sum" gives the sum of pixel values, so change from [0,255] to [0,1]
     bin_mask = np.where(bin_mask > 0, 1, 0)
     all_ones = np.ones(bin_mask.shape[:2])
@@ -39,6 +43,7 @@ def coverage(img, bin_mask, geojson):
 
     # Save data to outputs
     for i, id_lbl in enumerate(ids):
+        observation_sample = label + str(id_lbl)
         # Save out pixel_count
         pixel_count = 0.0
         total = 1.0
@@ -46,16 +51,16 @@ def coverage(img, bin_mask, geojson):
             pixel_count = region_counts[i]["sum"]
         if total_region[i]["sum"] is not None:
             total = total_region[i]["sum"]
-        outputs.add_observation(sample=id_lbl, variable="pixel_count", trait="count",
+        outputs.add_observation(sample=observation_sample, variable="pixel_count", trait="count",
                                 method="rasterstats.zonal_stats", scale="pixels", datatype=int,
                                 value=pixel_count, label="pixels")
         # Scale and save out coverage in CRS units
-        outputs.add_observation(sample=id_lbl, variable="coverage", trait="coverage",
+        outputs.add_observation(sample=observation_sample, variable="coverage", trait="coverage",
                                 method="plantcv-geospatial.analyze.coverage",
                                 scale=img.metadata["crs"].linear_units, datatype=float,
                                 value=pixel_count * (gsd_x * gsd_y), label="square " + img.metadata["crs"].linear_units)
         # Save out percent coverage
-        outputs.add_observation(sample=id_lbl, variable="percent_coverage", trait="percentage",
+        outputs.add_observation(sample=observation_sample, variable="percent_coverage", trait="percentage",
                                 method="rasterstats.zonal_stats", scale="none", datatype=float,
                                 value=pixel_count/total, label="none")
 
