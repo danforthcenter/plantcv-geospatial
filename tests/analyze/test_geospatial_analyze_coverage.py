b8d5f01fcaeb68b451fa9659b427f9a5752d95cc
HaleySchuhl <haleyschuhl@gmail.com>
2025-11-07 11:39:18 -0600

    fix tests to fully cover lines of code

--- tests/analyze/test_geospatial_analyze_coverage.py
+++ tests/analyze/test_geospatial_analyze_coverage.py
@@ -17,8 +17,8 @@ def test_coverage(debug, tmpdir, test_data):
     bin_mask = img.array_data[:, :, 2]  # Make a grayscale img to use as the mask
     # Debug mode
     params.debug = debug
-    _ = analyze_coverage(img=img, bin_mask=bin_mask, geojson=test_data.square_crop, label="test")
-    assert outputs.observations["test_0"]["percent_coverage"]["value"] <= 1
+    _ = analyze_coverage(img=img, bin_mask=bin_mask, geojson=test_data.square_crop)
+    assert outputs.observations["default_0"]["percent_coverage"]["value"] <= 1
 
 
 def test_coverage_with_geo_ids(test_data):
