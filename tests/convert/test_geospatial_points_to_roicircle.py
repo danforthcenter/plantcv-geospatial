5f01852bd8cad81cffbcfefc25134c6d486a279d
k034b363 <k034b363@gmail.com>
2026-03-04 12:00:50 -0600

    Add convert subfolder

--- tests/convert/test_geospatial_points_to_roicircle.py
+++ tests/convert/test_geospatial_points_to_roicircle.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 import joblib
-from plantcv.geospatial import points2roi_circle
+from plantcv.geospatial.convert import points_to_roi_circle
 
 
-def test_geospatial_points2roi_circle(test_data):
+def test_geospatial_points_to_roi_circle(test_data):
     """Test for plantcv-geospatial."""
     # read in small 3-band tif image
     img = joblib.load(test_data.rgb_pickled)
-    rois = points2roi_circle(img=img, geojson=test_data.pts_geojson, radius=0.5)
+    rois = points_to_roi_circle(img=img, geojson=test_data.pts_geojson, radius=0.5)
     assert np.all(rois.contours[0][0][0] == np.array([119, 170]))
