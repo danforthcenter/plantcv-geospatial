ebd4caa34118f0473f8c0cb9da793d2154d2e60f
Malia Gehan <mgehan@danforthcenter.org>
2026-03-04 10:25:20 -0600

    update function to add fatal error if points is empty

--- plantcv/geospatial/center_grid_rois.py
+++ plantcv/geospatial/center_grid_rois.py
@@ -1,38 +1,44 @@
 # Takes a napari viewer object with polygons and turns the points into rois
 
-import shapely
-from plantcv.plantcv.roi import multi, circle
+import cv2
+import numpy as np
+from shapely import centroid
 from shapely.geometry import Polygon
+from plantcv.plantcv import fatal_error
+from plantcv.plantcv.roi import multi, circle
 
 
 def center_grid_rois(img, viewer, radius=10, layername="Shapes"):
     """Create circular ROIs centered at the centroids of polygon shapes.
+    Inputs:
+    img         = Input image on which the ROIs will be drawn.
+    viewer      = Napari viewer object containing a shapes layer with polygons.
+                  Typically the output from napari_polygon_grid.
+    radius      = Radius of the circular ROIs in pixels (optional, default=10).
+    layername   = Name of the shapes layer to use (optional, default="Shapes").
 
-    Extracts the centroid from each polygon in a shapes layer and creates
-    circular regions of interest (ROIs) at those points.
+    Returns:
+    rois        = Multi-ROI object containing all circular regions of interest
+                  and their hierarchical relationships.
 
-    Parameters
-    ----------
-    img : numpy.ndarray
-        Input image on which the ROIs will be drawn.
-    viewer : napari.Viewer
-        Napari viewer object containing a shapes layer with polygons.
-        Typically the output from napari_polygon_grid.
-    radius : int
-        Radius of the circular ROIs in pixels.
-    layername : str, optional
-        Name of the shapes layer to use. Default is "Shapes".
-
-    Returns
-    -------
-    rois : plantcv.plantcv.classes.Objects
-        Multi-ROI object containing all circular regions of interest and
-        their hierarchical relationships.
+    :param img: numpy.ndarray
+    :param viewer: napari.Viewer
+    :param radius: int
+    :param layername: str
+    :return rois: plantcv.plantcv.classes.Objects
     """
     points_list = []
     for i in viewer.layers[layername].data:
-        point = shapely.centroid(Polygon(i))
+        point = centroid(Polygon(i))
         points_list.append((point.coords[0][1], point.coords[0][0]))
+
+    if not points_list:
+        fatal_error(f"No shapes found in layer '{layername}'.")
+
+    debug_img = np.copy(img)
+    for x, y in points_list:
+        cv2.circle(debug_img, (int(x), int(y)), radius, (255, 0, 0), 2)
+
     if len(points_list) > 1:
         return multi(img=img, coord=points_list, radius=radius)
     return circle(img=img, x=int(points_list[0][0]),
