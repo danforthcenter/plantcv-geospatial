6042725882aa6c74cab8f1b3a921f3af9e91d7ae
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-05 08:23:06 -0600

    docstring and writing to cleaner filename

--- plantcv/geospatial/convert/points_to_roi.py
+++ plantcv/geospatial/convert/points_to_roi.py
@@ -1,4 +1,5 @@
 # Transform georeferenced GeoJSON/shapefile points into python coordinates
+import os
 import numpy as np
 from plantcv.geospatial.transform_polygons import transform_polygons
 from plantcv.geospatial._helpers import _transform_geojson_crs
@@ -8,26 +9,28 @@ from plantcv.plantcv import Objects
 def points_to_roi_circle(img, geojson, radius):
     """Takes a points-type shapefile/GeoJSON and transforms circular ROIs,
     saves these out to a new geoJSON file and creates ROI Objects instances
-    Inputs:
-    img:        A spectral object from read_geotif.
-    geojson:    Path to the shape file containing the points.
-    radius:     Radius of circular ROIs to get created,
-                in units matching the coordinate system (CRS) of the image
-                e.g. meters
+
+    Parameters:
+    -----------
+    img:        plantcv.Spectral_data
+        A spectral object from read_geotif.
+    geojson:    str
+        Path to the shape file containing the points.
+    radius:     float
+        Radius of circular ROIs to get created,
+        in units matching the coordinate system (CRS) of the image
+        e.g. meters
 
     Returns:
-    rois:       List of circular ROIs (plantcv Objects class instances)
-
-    :param img: [spectral object]
-    :param geojson: str
-    :param radius: float
-    :return rois: list
+    --------
+    rois:       list
+        List of circular ROIs (plantcv Objects class instances)
     """
     gdf = _transform_geojson_crs(img=img, geojson=geojson)
 
     gdf['geometry'] = gdf.geometry.buffer(radius)
 
-    buffered_geojson = geojson + '_circles.geojson'
+    buffered_geojson = os.path.splitext(geojson)[0] + '_circles.geojson'
     gdf.to_file(buffered_geojson, driver='GeoJSON')
 
     geo_rois = transform_polygons(img=img, geojson=buffered_geojson)
@@ -36,15 +39,18 @@ def points_to_roi_circle(img, geojson, radius):
 
 
 def _points2roi(roi_list):
-    """
-    Helper that takes ROI contour coordinates and populates
+    """Helper that takes ROI contour coordinates and populates
     a plantcv Objects class instance
 
-    Inputs:
-    roi_list  = List of ROI contours from georeferenced origin
+    Parameters:
+    -----------
+    roi_list  = list
+        List of ROI contours from georeferenced origin
 
     Returns:
-    rois    = grouped contours list
+    --------
+    rois    = plantcv.Objects instance
+        grouped contours list
 
     :param roi_list: list
     :return rois: plantcv.plantcv.classes.Objects
