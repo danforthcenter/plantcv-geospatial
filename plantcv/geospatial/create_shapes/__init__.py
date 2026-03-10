08a33ef783bcde728c948d915fe27d6104c1e230
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-10 08:27:25 -0500

    export new name

--- plantcv/geospatial/create_shapes/__init__.py
+++ plantcv/geospatial/create_shapes/__init__.py
@@ -1,5 +1,5 @@
-from plantcv.geospatial.create_shapes.flexible import flexible
+from plantcv.geospatial.create_shapes.grid_from_points import grid_from_points
 from plantcv.geospatial.create_shapes.grid import grid
 
 
-__all__ = ["flexible", "grid"]
+__all__ = ["grid_from_points", "grid"]
