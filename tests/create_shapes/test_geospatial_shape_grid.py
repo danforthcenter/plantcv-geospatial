b2b040eb0c144c5f00393a14d658203caf46bb30
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-10 08:06:10 -0500

    move tests to new create_shapes submodule name

--- tests/create_shapes/test_geospatial_shape_grid.py
+++ tests/create_shapes/test_geospatial_shape_grid.py
@@ -1,6 +1,6 @@
 """Tests for geospatial.transform_points"""
 
-from plantcv.geospatial.shapes import grid as shape_grid
+from plantcv.geospatial.create_shapes import grid
 import joblib
 import os
 
@@ -11,6 +11,6 @@ def test_geospatial_shape_grid(test_data, tmpdir):
     filename = os.path.join(cache_dir, 'test_out.geojson')
     # Read in test data
     img = joblib.load(test_data.rgb_pickled)
-    _ = shape_grid(img=img, field_corners_path=test_data.point_crop, out_path=filename, num_ranges=2, num_columns=16,
-                   num_rows=4, range_length=3.6576, row_length=0.9144, range_spacing=0, column_spacing=0)
+    _ = grid(img=img, field_corners_path=test_data.point_crop, out_path=filename, num_ranges=2, num_columns=16,
+             num_rows=4, range_length=3.6576, row_length=0.9144, range_spacing=0, column_spacing=0)
     assert os.path.exists(filename)
