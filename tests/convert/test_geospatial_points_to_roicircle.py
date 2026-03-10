d9df495005a074246c464e4b449ee711b6e8baaa
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-10 15:12:13 -0500

    Revert "update tests"
    This reverts commit 66026bb756559f5a35c4e521af48e3cf996604a1.

--- tests/convert/test_geospatial_points_to_roicircle.py
+++ tests/convert/test_geospatial_points_to_roicircle.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 import joblib
-from plantcv.geospatial.convert.to_roi import to_roi
+from plantcv.geospatial.convert import points_to_roi_circle
 
 
-def test_geospatial_points_to_roi(test_data):
+def test_geospatial_points_to_roi_circle(test_data):
     """Test for plantcv-geospatial."""
     # read in small 3-band tif image
     img = joblib.load(test_data.rgb_pickled)
-    rois = to_roi(img=img, geojson=test_data.pts_geojson, radius=0.5)
+    rois = points_to_roi_circle(img=img, geojson=test_data.pts_geojson, radius=0.5)
     assert np.all(rois.contours[0][0][0] == np.array([119, 170]))
