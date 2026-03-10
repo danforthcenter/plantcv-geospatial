5b3c28fbb3cfef6f96353d4c8d59d1bb9809cba9
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-10 09:07:11 -0500

    move napari_polygon_grid to create_shapes

--- plantcv/geospatial/create_shapes/__init__.py
+++ plantcv/geospatial/create_shapes/__init__.py
@@ -1,6 +1,6 @@
 from plantcv.geospatial.create_shapes.grid_from_points import grid_from_points
 from plantcv.geospatial.create_shapes.grid import grid
 from plantcv.geospatial.create_shapes.napari_grid import napari_grid
+from plantcv.geospatial.create_shapes.napari_polygon_grid import napari_polygon_grid
 
-
-__all__ = ["grid_from_points", "grid", "napari_grid"]
+__all__ = ["grid_from_points", "grid", "napari_grid", "napari_polygon_grid"]
