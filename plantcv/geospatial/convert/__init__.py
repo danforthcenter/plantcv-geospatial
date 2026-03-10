10e2a380b4b96d1734775a6c32bbdf4263e600cd
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-10 15:02:21 -0500

    move points_to_roi to to_roi

--- plantcv/geospatial/convert/__init__.py
+++ plantcv/geospatial/convert/__init__.py
@@ -1,6 +1,6 @@
 from plantcv.geospatial.convert.points import points
 from plantcv.geospatial.convert.shapes import shapes
-from plantcv.geospatial.convert.points_to_roi import points_to_roi_circle
+from plantcv.geospatial.convert.to_roi import to_roi
 
 
-__all__ = ["points", "shapes", "points_to_roi_circle"]
+__all__ = ["points", "shapes", "to_roi"]
