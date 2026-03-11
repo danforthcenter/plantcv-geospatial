0de09ca223210e0f6d7df9fd38d3ac0b77efece7
k034b363 <k034b363@gmail.com>
2026-03-11 09:35:03 -0500

    changed grid_from_points to grid_from_coords

--- plantcv/geospatial/create_shapes/__init__.py
+++ plantcv/geospatial/create_shapes/__init__.py
@@ -1,5 +1,5 @@
-from plantcv.geospatial.create_shapes.grid_from_points import grid_from_points
+from plantcv.geospatial.create_shapes.grid_from_coords import grid_from_coords
 from plantcv.geospatial.create_shapes.grid import grid
 
 
-__all__ = ["grid_from_points", "grid"]
+__all__ = ["grid_from_coords", "grid"]
