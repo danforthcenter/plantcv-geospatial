40ea88b1df8be741af4f941934843e10f97146ce
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-12 13:40:49 -0500

    update argument names

--- plantcv/geospatial/convert/shapes.py
+++ plantcv/geospatial/convert/shapes.py
@@ -5,21 +5,21 @@ from shapely.geometry import Polygon, mapping
 from plantcv.geospatial.convert.points import _geojson_to_points
 
 
-def shapes(frm, to=None, img=None, shapetype="polygon", layername="Shapes"):
+def shapes(source, dest=None, img=None, shapetype="polygon", layername="Shapes"):
     """Use clicks from a Napari or plantcv-annotate viewer to output a geojson shapefile.
 
     Parameters:
     -----------
-    frm : str, Napari.viewer
+    source : str, Napari.viewer
         Either a geojson file or the viewer used to make the clicks.
         A geojson file will return a list of coordinates from that geojson.
         A Napari viewer will save and return a geojson object.
-    to : str,
-        Path to save to a geojson file to save if frm is a Napari viewer or Points object.
-        Defaults to None, only required if 'frm' is a Napari view or Points object.
+    dest : str,
+        Path to save to a geojson file to save if source is a Napari viewer or Points object.
+        Defaults to None, only required if 'source' is a Napari view or Points object.
     img : plantcv.plantcv.classes.Spectral_data
         The image used for clicking on points, should be from read_geotif.
-        Defaults to None, only required if 'frm' is a Napari view or Points object.
+        Defaults to None, only required if 'source' is a Napari view or Points object.
     shapetype: str, optional
         Geometry type from Napari viewer shape layer desired for geojson output, defaults to "polygon."
     shapename: str, optional
@@ -27,14 +27,14 @@ def shapes(frm, to=None, img=None, shapetype="polygon", layername="Shapes"):
 
     Returns:
     --------
-    list or dict, if frm is a str then returns a list of X,Y coordinates.
-        If frm is a Napari.viewer then a dictionary of geojson data is returned.
+    list or dict, if source is a str then returns a list of X,Y coordinates.
+        If source is a Napari.viewer then a dictionary of geojson data is returned.
     """
-    if isinstance(frm, str):
+    if isinstance(source, str):
         # a shape here is just a collection of points, so we use the points helper
-        return _geojson_to_points(filename=frm)
-    # otherwise frm should be a napari viewer
-    return _shape_to_geojson(img=img, viewer=frm, out_path=to, shapetype=shapetype, layername=layername)
+        return _geojson_to_points(filename=source)
+    # otherwise source should be a napari viewer
+    return _shape_to_geojson(img=img, viewer=source, out_path=dest, shapetype=shapetype, layername=layername)
 
 
 def _shape_to_geojson(img, viewer, out_path, shapetype="polygon", layername="Shapes"):
