1b03008a83a4f80558f85d64f6f5835c3dab5e90
k034b363 <k034b363@gmail.com>
2026-03-12 15:52:53 -0500

    Convert geojson coordinates to numpy

--- docs/convert_points.md
+++ docs/convert_points.md
@@ -1,13 +1,13 @@
 ## Convert Points between geojson and lists
 
-Using a Napari or PlantCV-annotate viewer object with clicked points, output a shapefile.
+Using a Napari or PlantCV-annotate viewer object with clicked points, output a shapefile. Or read in a Point- or MultiPoint-type geojson shapefile and output a list of coordinates in numpy space. 
 
-**plantcv.geospatial.convert.points**(*source, dest=None, img=None*)
+**plantcv.geospatial.convert.points**(*img, source, dest=None*)
 
 - **Parameters:**
+    - img - Spectral image object, likely read in with [`geo.read_geotif`](read_geotif.md). If `source` is the path to a shapefile, the metadata of this image will be used to convert points to numpy coordinates.
     - source - str, Napari.viewer, or plantcv.annotate.classes.Points object. If this is an str then it should be a path to a geojson file to read points from. A Napari.viewer or Points object will be saved to a geojson specified by `dest`.
 	- dest - str, Path to save a geojson file if `source` is a Napari.viewer or Points object. This is not required if `source` is a geojson file path.
-    - img - Spectral image object, likely read in with [`geo.read_geotif`](read_geotif.md).  This is not required if `source` is a geojson file path.
 
 
 - **Context:**
--- plantcv/geospatial/convert/points.py
+++ plantcv/geospatial/convert/points.py
@@ -5,20 +5,20 @@ import fiona
 from plantcv.plantcv.fatal_error import fatal_error
 
 
-def points(source, dest=None, img=None):
+def points(img, source, dest=None):
     """Convert between Points/Napari.viewer objects and geojson objects/files
 
     Parameters
     ----------
+    img : plantcv.plantcv.classes.Spectral_data
+        The image used for clicking on points, or that points from a file should be transformed to match, 
+        should be from read_geotif.
     source : str, Napari.viewer, or plantcv.annotate.classes.Points object.
-        Either a geojson file or the viewer used to make the clicks.
+        Either a geojson file or a viewer used to click points.
         A geojson file will return a list of coordinates from that geojson.
-        A Napari viewer or Points object will save and return a geojson object.
+        A Napari viewer or Points object will save a geojson and return a list of coordinates.
     dest : str,
         Path to save to a geojson file to save if source is a Napari viewer or Points object.
-        Defaults to None, only required if 'source' is a Napari view or Points object.
-    img : plantcv.plantcv.classes.Spectral_data
-        The image used for clicking on points, should be from read_geotif.
         Defaults to None, only required if 'source' is a Napari view or Points object.
 
     Returns:
@@ -32,7 +32,7 @@ def points(source, dest=None, img=None):
     """
     if isinstance(source, str):
         # if source is a string then it is path to a geojson file
-        return _geojson_to_points(filename=source)
+        return _geojson_to_points(img, filename=source)
     # otherwise, source should be a napari viewer or Points object
     return _points_to_geojson(img, viewer=source, out_path=dest)
 
@@ -84,19 +84,33 @@ def _points_to_geojson(img, viewer, out_path):
     return feature_collection
 
 
-def _geojson_to_points(filename):
-    """Extract coordinates from geojson
+def _geojson_to_points(img, filename):
+    """
+    Transform a points-type shapefile or GeoJSON into image pixel coordinates.
 
     Parameters
     ----------
-    filename : str, path to geojson file.
+    img : plantcv.plantcv.classes.Spectral_data
+        A spectral image object returned by ``read_geotif``.
+    filename : str
+        Path to the shapefile or GeoJSON file containing points.
 
-    Returns:
+    Returns
     -------
-    pts : list, list of X,Y coordinates from the geometry.coordinates of the geojson file.
+    coord : list of tuple of int
+        Pixel coordinates as a list of ``(col, row)`` integer tuples,
+        one per point feature in the input file.
     """
-    pts = []
-    with fiona.open(filename, "r") as shapefile:
+    geo_transform = img.metadata["transform"]
+
+    coord = []
+    with fiona.open(filename, 'r') as shapefile:
         for row in shapefile:
-            pts.append(row['geometry']['coordinates'])
-    return pts
+            if type((row.geometry["coordinates"])) is list:
+                pixel_point = ~(geo_transform) * (row.geometry["coordinates"][0])
+            if type((row.geometry["coordinates"])) is tuple:
+                pixel_point = ~(geo_transform) * (row.geometry["coordinates"])
+            rounded = (int(pixel_point[0]), int(pixel_point[1]))
+            coord.append(rounded)
+
+    return coord
