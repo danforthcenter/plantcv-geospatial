4be6fd5d2fe79ebbdcb242c7b1520ff0ded8b415
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-03-13 11:59:21 -0500

    change inits for new location

--- plantcv/geospatial/create_shapes/__init__.py
+++ plantcv/geospatial/create_shapes/__init__.py
@@ -1,4 +1,6 @@
 from plantcv.geospatial.create_shapes.grid_from_coords import grid_from_coords
 from plantcv.geospatial.create_shapes.auto_grid import auto_grid
+from plantcv.geospatial.create_shapes.interactive_shapes import InteractiveShapes
 
-__all__ = ["grid_from_coords", "auto_grid"]
+
+__all__ = ["grid_from_coords", "auto_grid", "InteractiveShapes"]
