8222c482fbbd133d39db4ac7807ff575d8434e82
k034b363 <k034b363@gmail.com>
2026-03-23 14:38:54 -0500

    Create images.py

--- plantcv/geospatial/images.py
+++ plantcv/geospatial/images.py
@@ -1,6 +1,7 @@
 # Sets up image classes as subclasses of numpy.ndarray
 
 import numpy as np
+import affine
 
 class Image(np.ndarray):
     """The generic Image class extends the np.ndarray class by adding attributes."""
@@ -28,11 +29,14 @@ class GEO(Image):
     """Subclass of Image for geospatial images."""
     
     def __new__(cls, input_array: np.ndarray, filename: str, wavelengths: list, 
-                crs : str, transform : affine.Affine):
+                default_wavelengths : list, channels : list, crs : str, 
+                transform : affine.Affine):
         # Create an instance of Image with default attributes
         obj = Image.__new__(cls, input_array, filename)
         # Add HSI-specific attributes
         obj.wavelengths = wavelengths
+        obj.default_wavelengths = default_wavelengths
+        obj.channels = channels
         obj.crs = crs
         obj.transform = transform
         return obj
@@ -43,19 +47,47 @@ class GEO(Image):
     def get_wavelength(self, wavelength):
         idx = np.abs(np.array(self.wavelengths) - wavelength).argmin()
         obj = super(GEO, self).__getitem__(np.s_[:, :, idx])
-        obj.wavelengths = [self.wavelengths[idx]]
-        obj.min_wavelength = np.min(obj.wavelengths)
-        obj.max_wavelength = np.max(obj.wavelengths)
         return obj
 
     def _create_thumb(self):
-        if len(self.default_wavelengths) == 3:
-            thumb = BGR(input_array=np.dstack([self.get_wavelength(self.default_wavelengths[0]),
-                                               self.get_wavelength(self.default_wavelengths[1]),
-                                               self.get_wavelength(self.default_wavelengths[2])]),
-                        filename=self.filename)
-        else:
-            thumb = GRAY(input_array=self.get_wavelength(self.default_wavelengths[0]),
-                         filename=self.filename)
+        thumb = np.dstack([self.get_wavelength(self.default_wavelengths[0]),
+                           self.get_wavelength(self.default_wavelengths[1]),
+                           self.get_wavelength(self.default_wavelengths[2])])
+        return thumb
+
+class DSM(Image):
+    """Subclass of Image for digital surface models."""
+    
+    def __new__(cls, input_array: np.ndarray, filename: str, crs : str, 
+                transform : affine.Affine, cutoff : float):
+        # Create an instance of Image with default attributes
+        obj = Image.__new__(cls, input_array, filename)
+        # Add HSI-specific attributes
+        obj.crs = crs
+        obj.transform = transform
+        obj.cutoff = cutoff
+        return obj
+
+    def __init__(self, **kwargs):
+        self.data_array = self._gray_cutoff()
+        self.thumb = self._create_thumb()
+        
+    def _gray_cutoff(self):
+        img_copy = np.squeeze(self.input_array)
+        if self.cutoff is not None :
+            quantile = np.quantile(img_copy, self.cutoff)
+            img_copy[img_copy >= quantile] = np.nan
+        return img_copy
+
+    def _create_thumb(self):
+        img_copy = self.data_array
+        # Change nodata values to Nan
+        img_copy[img_copy == min(np.unique(img_copy))] = np.nan
+        # Stretch values to min/max for visualization
+        img_copy = 255*((img_copy - np.nanmin(img_copy)) / (np.nanmax(img_copy) - np.nanmin(img_copy)))
+        # Return nodata values to 0
+        img_copy = np.nan_to_num(img_copy, nan=0.0)
+        # Convert to uint8
+        thumb = img_copy.astype(np.uint8)
         return thumb
     
