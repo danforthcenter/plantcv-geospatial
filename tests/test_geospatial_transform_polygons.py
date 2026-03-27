621351f393c43889b0d036d2c6969ac41269961a
k034b363 <k034b363@gmail.com>
2026-03-27 09:06:54 -0500

    Change to dill in test for transform polygons

--- tests/test_geospatial_transform_polygons.py
+++ tests/test_geospatial_transform_polygons.py
@@ -1,13 +1,14 @@
 """Tests for geospatial.transform_polygons"""
 
-import joblib
+import dill as pickle
 from plantcv.geospatial import transform_polygons
 
 
 def test_geospatial_transform_polygons(test_data):
     """Test for plantcv-geospatial."""
     # read in small 5-band tif image
-    img = joblib.load(test_data.geo_pickled)
+    with open(test_data.geo_pickled, "rb") as f:
+        img = pickle.load(f)
     coords = transform_polygons(img=img, geojson=test_data.square_crop)
     assert coords == [[[196, 115], [145, 78], [114, 120], [165, 157]]]
 
@@ -15,6 +16,7 @@ def test_geospatial_transform_polygons(test_data):
 def test_geospatial_transform_single_polygons(test_data):
     """Test for plantcv-geospatial."""
     # read in small 5-band tif image
-    img = joblib.load(test_data.geo_pickled)
+    with open(test_data.geo_pickled, "rb") as f:
+        img = pickle.load(f)
     coords = transform_polygons(img=img, geojson=test_data.multipolygon)
     assert coords[0] == [[167, 28], [200, 52], [176, 85], [143, 60]]
