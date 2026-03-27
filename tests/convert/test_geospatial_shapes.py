a87238924143aa6f39ab71961e8bbe7cb0059abe
k034b363 <k034b363@gmail.com>
2026-03-27 09:04:35 -0500

    Add dill to dependencies

--- tests/convert/test_geospatial_shapes.py
+++ tests/convert/test_geospatial_shapes.py
@@ -2,14 +2,15 @@
 
 import os
 import napari
-import joblib
+import dill as pickle
 from plantcv.geospatial.convert.shapes import shapes
 
 
 def test_geospatial_shapes_from_napari_to_geojson(test_data, tmpdir):
     """Test for plantcv-geospatial."""
     cache_dir = tmpdir.mkdir("cache")
-    img = joblib.load(test_data.geo_pickled)
+    with open(test_data.geo_pickled, "rb") as f:
+        img = pickle.load(f)
     viewer = napari.Viewer(show=False)
     viewer.add_image(img.thumb)
     viewer.add_shapes([[2, 3], [3, 3], [3, 4], [2, 4]], shape_type="polygon")
@@ -21,7 +22,8 @@ def test_geospatial_shapes_from_napari_to_geojson(test_data, tmpdir):
 def test_geospatial_shapes_to_geojson_badfilename(test_data, tmpdir):
     """Test for plantcv-geospatial."""
     cache_dir = tmpdir.mkdir("cache")
-    img = joblib.load(test_data.geo_pickled)
+    with open(test_data.geo_pickled, "rb") as f:
+        img = pickle.load(f)
     viewer = napari.Viewer(show=False)
     viewer.add_image(img.thumb)
     viewer.add_shapes([[2, 3], [3, 3], [3, 4], [2, 4]], shape_type="polygon")
@@ -32,6 +34,7 @@ def test_geospatial_shapes_to_geojson_badfilename(test_data, tmpdir):
 
 def test_geospatial_shapes_from_geojson_to_list(test_data):
     """Test for plantcv-geospatial."""
-    img = joblib.load(test_data.geo_pickled)
+    with open(test_data.geo_pickled, "rb") as f:
+        img = pickle.load(f)
     l = shapes(img=img, source=test_data.square_crop)
     assert len(l[0]) == 4
