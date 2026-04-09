15498da45c7671967eb8621c2764e0bc7f62d1f4
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-04-09 16:02:42 -0500

    coercing nodata to 0

--- plantcv/geospatial/images.py
+++ plantcv/geospatial/images.py
@@ -46,6 +46,7 @@ class GEO(Image):
                  default_wavelengths: list, crs: str, transform: affine.Affine, nodata: float):
         super().__init__()
         self.thumb = self._create_thumb()
+        self.nodata = 0
 
     def __array_finalize__(self, obj):
         super().__array_finalize__(obj)
@@ -107,6 +108,7 @@ class DSM(Image):
         super().__init__()
         self.data_array = self._gray_cutoff()
         self.thumb = self._create_thumb()
+        self.nodata = 0
 
     def __array_finalize__(self, obj):
         super().__array_finalize__(obj)
