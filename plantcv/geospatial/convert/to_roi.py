e3836a672528e70993dc98e273b42214a54ec2ea
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-09-23 14:59:34 -0500

    refactor to_roi to work with inputs besides geojson

--- plantcv/geospatial/convert/to_roi.py
+++ plantcv/geospatial/convert/to_roi.py
@@ -2,13 +2,13 @@
 import os
 import numpy as np
 from plantcv.geospatial.transform_polygons import transform_polygons
-from plantcv.geospatial._helpers import _transform_geojson_crs
+from plantcv.geospatial._helpers import _to_gdf, _gdf_to_pixel_polygons
 from plantcv.plantcv.fatal_error import fatal_error
 from plantcv.plantcv.classes import Objects
 from plantcv.plantcv.roi.roi_methods import _draw_roi
 
 
-def to_roi(img, geojson, radius=None):
+def to_roi(img, source, radius=None, layername="Shapes"):
     """Takes a points- or polygon-type shapefile/GeoJSON and transforms to ROIs,
     and creates ROI Objects instances. If points, saves circular ROIs out to a new geoJSON file.
 
@@ -16,94 +16,36 @@ def to_roi(img, geojson, radius=None):
     -----------
     img : plantcv.geospatial.images.GEO object
         A GEO image object returned by ``read_geotif``.
-    geojson : str
-        Path to the shape file containing the points or polygons.
+    source : str, InteractiveShapes, or Napari Viewer
+        This should provide points/polygons.
+        An str will be treated as a filepath to a geojson file.
+        An InteractiveShapes instance will have the viewer used.
+        A Napari Viewer will have the specified layername used to make ROIs.
     radius : optional float
         If provided, then points from the geojson will be treated as centers
         of circular ROIs with this radius
-        in units matching the coordinate system (CRS) of the image
-        e.g. meters
+        in units matching the coordinate system (CRS) of the image.
+        This should only be used with Point/MultiPoint layers.
 
     Returns:
     --------
     rois : list
         List of circular ROIs (plantcv Objects class instances)
     """
+    gdf = _to_gdf(img, source, layername)
+    geom_types = set(gdf.geom_type)
     if radius is not None:
-        rois = _points_to_circular_rois(img, geojson, radius)
-    else :
-        rois = _polygon_to_roi(img, geojson)
+        if not geom_types <= {"Point", "MultiPoint"}:
+            fatal_error("A radius was specified but can only be used with Point and MultiPoint geometries. " +
+                        "Source contains '" + ", ".join(gdf.geom_type.unique()) + "'"
+                        )
+        gdf = gdf.assign(geometry=gdf.geometry.buffer(radius))
+    elif not geom_types <= {"Polygon", "MultiPolygon"}:
+        fatal_error("Non-polygon ROIs require a radius. Source is geom_type '" +
+                    ", ".join(gdf.geom_type.unique()) + "'")
 
-    # Draw rois if requested
+    rois = Objects()
+    for polygon in _gdf_to_pixel_polygons(img, gdf):
+        rois.append(contour=[np.array(polygon)], h=[])
     _draw_roi(img=img.thumb, roi_contour=rois)
-
-    return rois
-
-
-def _points_to_circular_rois(img, geojson, radius):
-    """Make circular ROIs from points in a geojson file
-
-    Parameters:
-    -----------
-    img : plantcv.geospatial.images.GEO object
-        A GEO image object returned by ``read_geotif``.
-    geojson : str
-        Path to the shape file containing the points.
-    radius : float
-        Points from the geojson will be treated as centers
-        of circular ROIs with this radius
-        in units matching the coordinate system (CRS) of the image
-        e.g. meters
-
-    Returns:
-    --------
-    rois : list
-        List of circular ROIs (plantcv Objects class instances)
-    """
-    gdf = _transform_geojson_crs(img=img, geojson=geojson)
-    # check that these are points, not polygons
-    if "Point" not in gdf.geom_type.unique()[0]:
-        fatal_error("Circular ROIs can only be specified with points layers, geojson file is geom_type '" +
-                    ", ".join(gdf.geom_type.unique()) + "'")
-
-    gdf['geometry'] = gdf.geometry.buffer(radius)
-
-    buffered_geojson = os.path.splitext(geojson)[0] + '_circles.geojson'
-    gdf.to_file(buffered_geojson, driver='GeoJSON')
-
-    geo_polygons = transform_polygons(img=img, geojson=buffered_geojson)
-    rois = Objects()
-    for polygon in geo_polygons:
-        rois.append(contour=[np.array(polygon)], h=[])
-
-    return rois
-
-
-def _polygon_to_roi(img, geojson):
-    """Make ROIs from polygons in a geojson file
-
-    Parameters:
-    -----------
-    img : plantcv.geospatial.images.GEO object
-        A GEO image object returned by ``read_geotif``.
-    geojson : str
-        Path to the shape file containing the polygons.
-
-    Returns:
-    --------
-    rois : list
-        List of polygon ROIs (plantcv Objects class instances)
-    """
-    gdf = _transform_geojson_crs(img=img, geojson=geojson)
-    # check that these are polygons, not points
-    if "Polygon" not in gdf.geom_type.unique()[0]:
-        fatal_error("Polygon ROIs can only be specified with polygon layers, geojson file is geom_type '" +
-                    ", ".join(gdf.geom_type.unique()) + "'")
-
-    geo_polygons = transform_polygons(img=img, geojson=geojson)
-
-    rois = Objects()
-    for polygon in geo_polygons:
-        rois.append(contour=[np.array(polygon)], h=[])
-
     return rois
